356d31c36df84df41fa1d5244dca62354a97536f
Naoya Akimoto <akitkat.cook.dx@gmail.com>
2021-09-03 13:41:45 +0900

    remove unneeded file.

--- src/reserved-methods.js
+++
@@ -1,9 +0,0 @@
-export const onOpen = (e) => {
-  SpreadsheetApp.getUi()
-    .createAddonMenu()
-    .addItem('ホテル一覧取得 by hotelNo', 'fetchAllByHotelNos')
-    .addItem('他ID取得', 'fetchAllOtherIds')
-    .addItem('他情報取得', 'fetchAllOtherDataByIds')
-    .addItem('掲載済みホテル一覧取得', 'fetchAllPublishedHotels')
-    .addToUi()
-}
