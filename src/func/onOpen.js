0a5110ec69d61de362c7d5183f1f2154fc031def
Naoya Akimoto <akitkat.cook.dx@gmail.com>
2021-09-03 13:41:57 +0900

    edit onopen func.

--- src/func/onOpen.js
+++ src/func/onOpen.js
@@ -2,5 +2,6 @@ export default () => {
   SpreadsheetApp.getUi()
     .createAddonMenu()
     .addItem('ホテル一覧取得 by hotelNo', 'fetchAllByHotelNoList')
+    .addItem('他情報取得', 'fetchAllOtherData')
     .addToUi();
 };
