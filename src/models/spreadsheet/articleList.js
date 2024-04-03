c9f19812fdaf0d45d97bdf2b571354968c5a216e
akitkat <akitkat.cook.dx@gmail.com>
2024-04-03 20:37:52 +0900

    Add ids and title to articleList constructor

--- src/models/spreadsheet/articleList.js
+++ src/models/spreadsheet/articleList.js
@@ -1,11 +1,13 @@
 export default class {
-  constructor(no, url, flg) {
+  constructor(no, url, flg, ids, title) {
     this.no = no;
     this.url = url;
     this.flg = flg;
+    this.ids = ids;
+    this.title = title;
   }
 
   toArray() {
-    return [this.no, this.url, this.flg];
+    return [this.no, this.url, this.flg, this.ids, this.title];
   }
 }
