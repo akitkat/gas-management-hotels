a0c11c42f83b08bb5acf1c19156b284025cc1256
akitkat <akitkat.cook.dx@gmail.com>
2024-04-03 20:38:05 +0900

    Add fetchHotelListByUrl function to onOpen menu

--- src/func/onOpen.js
+++ src/func/onOpen.js
@@ -5,5 +5,6 @@ export default () => {
     .addItem('ホテル一覧取得 by hotelNo', 'fetchAllByHotelNoList')
     .addItem('他情報取得', 'fetchAllOtherData')
     .addItem('公開済みホテル一覧更新', 'updatePublishedHotelList')
+    .addItem('記事作成', 'fetchHotelListByUrl')
     .addToUi();
 };
